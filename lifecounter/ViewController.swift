b0cf05551f77b328b319b1846df9a91fd5fc8fc3
George Lee <george10lee@gmail.com>
2025-04-18 17:25:41 -0700

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by George Lee on 4/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
